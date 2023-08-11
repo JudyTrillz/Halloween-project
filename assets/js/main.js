df7ce36e8df77d7c5912b33b6cc5ef57d2775c52
Judy Odiakose <ojudy007@gmail.com>
2023-08-11 05:52:51 +0100

    Final project with responsiveness and aos animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,63 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navClose = document.getElementById("nav-close");
+const navToggle = document.getElementById("nav-toggle");
 
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
-
+const navLink = document.querySelectorAll(".nav__link");
+navLink.forEach((n) =>
+  n.addEventListener("click", () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+  })
+);
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const header = document.getElementById("header");
+const headerAction = () => {
+  window.scrollY > 50
+    ? header.classList.add("bg__header")
+    : header.classList.remove("bg__header");
+};
+window.addEventListener("scroll", headerAction);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__item a[href*=' + sectionId + ']')
 
-/*=============== SHOW SCROLL UP ===============*/ 
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== SHOW SCROLL UP ===============*/
+const scrollBtn = document.getElementById("scroll-up");
+const scrollAction = () => {
+  window.scrollY > 300
+    ? scrollBtn.classList.add("show-scroll")
+    : scrollBtn.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollAction);
